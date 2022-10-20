21861443f7c317430c53d87d4c01fd7b64cd1841
WkRafal <wiezikrafal@gmail.com>
2022-10-20 10:16:03 +0200

    initialized commit

---
+++ src/main/java/com/github/WkRafal/plan_zadan/PlanZadanApplication.java
@@ -0,0 +1,13 @@
+package com.github.WkRafal.plan_zadan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanZadanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanZadanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/WkRafal/plan_zadan/PlanZadanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.WkRafal.plan_zadan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanZadanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
